69684f5e3d81349e21aa57eb5825399f0bad8031
Matthew Kimball <mekimba6@gmail.com>
2022-05-09 15:28:12 -0600

    adds comments

--- lib/bitly_challenge.rb
+++ lib/bitly_challenge.rb
@@ -1,10 +1,12 @@
 require 'csv'
 require 'json'
+#converts encodes to a hash
 rows = []
 CSV.foreach('encodes.csv', headers: true, converters: :all) do |row|
   rows << row.to_hash
 end
 
+#opens and parses the decodes file into JSON
 file = File.read('decodes.json')
 data_hash = JSON.parse(file)
 hash = Hash.new(0)
@@ -17,7 +19,7 @@ data_hash.each do |key, value|
     end
   end
 end
-
+#combines the two into a single JSON  
 hash = hash.sort_by { |_k, v| v }.reverse.to_h
 array = []
 hash.map do |key, value|
